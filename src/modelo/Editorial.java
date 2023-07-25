2ee4325d2f54e2cc09c025a249193d72186fbb20
crossbow <crossbow@DESKTOP-MHIED8R>
2023-07-25 14:12:55 +0200

    commit2

--- src/modelo/Editorial.java
+++ src/modelo/Editorial.java
@@ -1,5 +1,47 @@
 package modelo;
 
-public class Editorial {
+import java.util.Objects;
 
+public class Editorial {
+	private int codEditorial;
+	private String Nombre;
+	private int anio;
+	public Editorial(int codEditorial, String nombre, int anio) {
+		super();
+		this.codEditorial = codEditorial;
+		Nombre = nombre;
+		this.anio = anio;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public int getCodEditorial() {
+		return codEditorial;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(codEditorial);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Editorial other = (Editorial) obj;
+		return codEditorial == other.codEditorial;
+	}
+	
+	
 }
